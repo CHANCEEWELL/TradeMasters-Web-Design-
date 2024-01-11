fb0cf7894e61efc211bfb04a7a445abf344f0588
Chance Ewell <chance.shane.ewell@gmail.com>
2024-01-11 02:35:07 -0700

    Initialize main

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://Chanceewell.github.io/",
+    "name": "Chance Ewell",
+    "role": "Full-Stack Web Developer with a specialization in Front-end Design UX/UI",
+    "linkedinId":"Chanceewell",
+    "roleDescription": "",
+    "socialLinks":[
+      {
+        "name":"twitter",
+        "url":"https://twitter.com/ChanceEwell",
+        "className":"fa fa-twitter"
+      },
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/chanceewell/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/CHANCEEWELL",
+          "className":"fa fa-github"
+        },
+        {
+      
+        }
+      ],
+    "aboutme":"I am currently graduated from The University Of Utah's Full-Stack Development Bootcamp. I am specialized as a Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"chance.shane.ewell@gmail.com",
+    "website":"435-256-7527",
+    "education":[
+      {
+        "UniversityName":"The University Of Utah",
+        "specialization":"Full-Stack Web Development",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2021",
+        "Achievements":"Certificate of Graduation"
+      },
+
+    ],
+    "work":[
+      {
+        "CompanyName":"Busybusy Inc,",
+        "specialization":"Business Development Manager"
+      },
+      {
+        "CompanyName":"PrinterLogic",
+        "specialization":"Account Development Manager",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019"
+      },
+      {
+        "CompanyName":"HFB Technologies",
+        "specialization":"Front-end web design",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2018",
+        "Achievements":"Onboarding Specialist"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Chance is the definition of success. He puts his entire heart into EVERYTHING he does. There isn’t a person out there that can outwork this guy. He’s full of integrity, and will always be the first to show up and last to leave. He is kind, conscientious, and gritty. I hold Chance in the HIGHEST regard and recommend him without any reservation.",
+        "name":"Roger Comstock, Founder & CEO of Tuple Software"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
